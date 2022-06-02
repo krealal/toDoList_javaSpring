2f0c5fb48fd543b2619dff3f7a7da1ed10b7c69c
Kevin Real Alejo <kevinrealalejo@MacBook-Pro-de-Kevin.local>
2022-06-02 16:45:17 +0200

    create toDo model

---
+++ src/main/java/org/todolist/model/ToDo.java
@@ -0,0 +1,57 @@
+package org.todolist.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "toDoKRA")
+public class ToDo {
+    @Id
+    private String id;
+    private String toDo;
+    private String description;
+    private boolean isDone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToDo() {
+        return toDo;
+    }
+
+    public void setToDo(String toDo) {
+        this.toDo = toDo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo{" +
+                "id='" + id + '\'' +
+                ", toDo='" + toDo + '\'' +
+                ", description='" + description + '\'' +
+                ", isDone=" + isDone +
+                '}';
+    }
+}
+
+
