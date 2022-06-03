d00a772e14fd7695b22efb327a852c517a09b9a6
Kevin Real Alejo <kevinrealalejo@MBP-de-Kevin.home>
2022-06-03 13:07:20 +0200

    create ToDoListCRUD insted of main to execute spring app

---
+++ src/main/java/org/todolist/ToDoListCRUD.java
@@ -0,0 +1,13 @@
+package org.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListCRUD {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListCRUD.class, args);
+    }
+
+}
